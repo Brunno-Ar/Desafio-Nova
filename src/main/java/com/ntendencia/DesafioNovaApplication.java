340a70ee0ccf239fed3e044b5233a8d1ac131559
Bruno Araujo <brunnoaraujoc@gmail.com>
2023-03-19 20:52:12 -0300

    Primeiro commit

---
+++ src/main/java/com/ntendencia/DesafioNovaApplication.java
@@ -0,0 +1,13 @@
+package com.ntendencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntendencia/DesafioNovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntendencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
