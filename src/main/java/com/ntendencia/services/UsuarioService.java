81a90c1b99b582e439c1520a9cf0c621345fdcd4
Bruno Araujo <brunnoaraujoc@gmail.com>
2023-03-28 09:25:52 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/ntendencia/services/UsuarioService.java
+++ src/main/java/com/ntendencia/services/UsuarioService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ntendencia.domain.Usuario;
@@ -45,8 +48,13 @@ public class UsuarioService {
 			throw new DataIntegrityException("Não é possivel excluir usuarios que contém Endereço");
 		}
 	}
-	
+
 	public List<Usuario> findAll() {
 		return repository.findAll();
+	}
+
+	public Page<Usuario> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 }
