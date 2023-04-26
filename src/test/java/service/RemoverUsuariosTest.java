ef50efaf39b2f0f8626ef5bdb5e1d09933d57d09
Bruno Araujo <brunnoaraujoc@gmail.com>
2023-04-26 11:27:44 -0300

    Testes Unitarios para busca de usuario por id, busca todos os usuarios e deletar usuarios

--- src/test/java/service/RemoverUsuariosTest.java
+++ src/test/java/service/RemoverUsuariosTest.java
@@ -1,16 +1,12 @@
-package resource;
+package service;
 
-import com.ntendencia.domain.Usuario;
-import com.ntendencia.domain.enums.SexoUsuario;
 import com.ntendencia.repositories.EnderecoRepository;
 import com.ntendencia.repositories.UsuarioRepository;
 import com.ntendencia.services.UsuarioService;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,12 +14,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.util.Optional;
-
 @SpringBootTest(classes = UsuarioService.class)
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
-public class BuscarUsuariosTest {
+public class RemoverUsuariosTest {
 
     @InjectMocks
     private UsuarioService usuarioService;
@@ -34,17 +28,16 @@ public class BuscarUsuariosTest {
     @MockBean
     private EnderecoRepository enderecoRepository;
 
-//    @Before
-//    public void setup(){
-//        MockitoAnnotations.initMocks(this);
-//    }
-    @Test
-    public void buscaUsuarioPorIdTest(){
-        Usuario usuarioMockado = new Usuario (1, "Bruno", "123.123.123.23", "03/02/2003", SexoUsuario.MASCULINO);
-        Mockito.when(usuarioRepository.findById(usuarioMockado.getId())).thenReturn(Optional.of(usuarioMockado));
-        Usuario usuario = usuarioService.buscarUsuarioPorId(1);
-
-        Assertions.assertEquals(usuario, usuarioMockado);
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
     }
+
+    @Test
+    public void RemoverUsuariosTest() {
+        usuarioService.excluirUsuario(1);
+        Mockito.verify(usuarioRepository, Mockito.times(1)).deleteById(1);
+    }
+
 
 }
