3ec4faf00f851d04cd70a5b48a7ea778a292936a
Bruno Araujo <brunnoaraujoc@gmail.com>
2023-04-27 14:12:28 -0300

    Testes Unitarios para atualizar dados do usuario

--- src/test/java/service/AtualizarDadosUsuarioTest.java
+++ src/test/java/service/AtualizarDadosUsuarioTest.java
@@ -5,25 +5,22 @@ import com.ntendencia.domain.enums.SexoUsuario;
 import com.ntendencia.repositories.EnderecoRepository;
 import com.ntendencia.repositories.UsuarioRepository;
 import com.ntendencia.services.UsuarioService;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.util.List;
-
-import static org.mockito.Mockito.when;
-
 @SpringBootTest(classes = UsuarioService.class)
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
-public class BuscaTodosUsuariosTest {
+public class AtualizarDadosUsuarioTest {
+
 
     @InjectMocks
     private UsuarioService usuarioService;
@@ -40,14 +37,10 @@ public class BuscaTodosUsuariosTest {
     }
 
     @Test
-    public void BuscaTodosUsuariosTest() {
+    public void AtualizarDadosUsuarioTest(){
         Usuario usuarioMockado = new Usuario(1, "Bruno", "123.123.123.23", "03/02/2003", SexoUsuario.MASCULINO);
-        List<Usuario> usuarios = usuarioService.buscarTodosOsUsuarios();
-        usuarios.add(usuarioMockado);
-        when(usuarioRepository.findAll()).thenReturn(usuarios);
-
-        Assert.assertEquals(1, usuarios.size());
+        Usuario novoUsuarioMockaco = usuarioService.atualizarUsuario(usuarioMockado);
+        novoUsuarioMockaco = new Usuario(1, "Breno", "123.123.123.23", "16/03/2003", SexoUsuario.MASCULINO);
+        Mockito.verify(usuarioRepository, Mockito.times(1)).save(novoUsuarioMockaco);
     }
-
-
 }
