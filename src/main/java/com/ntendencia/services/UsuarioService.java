0ccad750b9a357cca7eb241755b0c422b922442b
Bruno Araujo <brunnoaraujoc@gmail.com>
2023-04-27 15:12:23 -0300

    Adicionando classe UsuarioServiceTeste com todos os teste e adicionando interface na clase UsuarioServiceImpl

--- src/main/java/com/ntendencia/services/UsuarioService.java
+++ src/main/java/com/ntendencia/services/UsuarioService.java
@@ -1,113 +1,24 @@
 package com.ntendencia.services;
 
-import com.ntendencia.DTO.UsuarioDTO;
-import com.ntendencia.DTO.UsuarioNewDTO;
-import com.ntendencia.domain.Cidade;
-import com.ntendencia.domain.Endereco;
 import com.ntendencia.domain.Usuario;
 import com.ntendencia.domain.enums.SexoUsuario;
-import com.ntendencia.repositories.EnderecoRepository;
-import com.ntendencia.repositories.UsuarioRepository;
-import com.ntendencia.services.exceptions.IntegridadeDeDadosException;
-import com.ntendencia.services.exceptions.ObjetoNaoEncontradoException;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class UsuarioService {
+public interface UsuarioService {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+    public Usuario buscarUsuarioPorId(Integer id);
 
-    @Autowired
-    private EnderecoRepository enderecoRepository;
+    public Usuario inserirNovoUsuario(Usuario obj);
 
-    private final ModelMapper modelMapper = new ModelMapper();
+    public Usuario atualizarUsuario(Usuario obj);
 
-    public Usuario buscarUsuarioPorId(Integer id) {
-        Optional<Usuario> usuario;
-        usuario = usuarioRepository.findById(id);
-        if (usuario == null) {
-            throw new ObjetoNaoEncontradoException(
-               Utils.getMensagemValidacao("usuario.nao.encontrado", id, Usuario.class.getName())
-            );
-        }
-        return usuario.orElse(null);
-    }
+    public void excluirUsuario(Integer id);
 
-    @Transactional
-    public Usuario inserirNovoUsuario(Usuario obj) {
-        obj.setId(null);
-        obj = usuarioRepository.save(obj);
-        enderecoRepository.saveAll(obj.getEnderecos());
-        return obj;
-    }
+    public List<Usuario> buscarTodosOsUsuarios();
 
-    public Usuario atualizarUsuario(Usuario obj) {
-        Usuario newObj = buscarUsuarioPorId(obj.getId());
-        atualizaObjeto(newObj, obj);
-        return usuarioRepository.save(newObj);
-    }
+    public Page<Usuario> buscaPaginadaDeUsuario(Integer page, Integer linesPerPage, String orderBy, String direction);
 
-    public void excluirUsuario(Integer id) {
-        buscarUsuarioPorId(id);
-        try {
-            usuarioRepository.deleteById(id);
-        } catch (DataIntegrityViolationException e) {
-            throw new IntegridadeDeDadosException(Utils.getMensagemValidacao("excluir.usuario.com.endereco"));
-        }
-    }
-
-    public List<Usuario> buscarTodosOsUsuarios() {
-
-        return usuarioRepository.findAll();
-    }
-
-
-    public Page<Usuario> buscaPaginadaDeUsuario(Integer page, Integer linesPerPage, String orderBy, String direction) {
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-        return usuarioRepository.findAll(pageRequest);
-    }
-
-    public Usuario inserirObjetoPeloDTO(UsuarioDTO objDto) {
-
-        return modelMapper.map(objDto, Usuario.class);
-    }
-
-    public Usuario inserirObjetoPeloDTO(UsuarioNewDTO objDto) {
-        Usuario usuario = modelMapper.map(objDto, Usuario.class);
-
-        Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
-
-        Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
-                objDto.getBairro(), objDto.getCep(), usuario, cid);
-
-        usuario.getEnderecos().add(end);
-
-        return usuario;
-    }
-
-    private void atualizaObjeto(Usuario newObj, Usuario obj) {
-        newObj.setNome(obj.getNome());
-    }
-
-    public List<Usuario> buscarUsuariosFiltrados(String nome, String cpf, String dataNascimento, SexoUsuario sexo) {
-        List<Usuario> usuario = usuarioRepository.findBy(nome, cpf, dataNascimento, sexo);
-        if (usuario == null) {
-            throw new ObjetoNaoEncontradoException(
-                    Utils.getMensagemValidacao("usuario.nao.encontrado", nome, Usuario.class.getName()));
-        }
-        return usuario;
-    }
-
-
+    public List<Usuario> buscarUsuariosFiltrados(String nome, String cpf, String dataNascimento, SexoUsuario sexo);
 }
---
+++ src/main/java/com/ntendencia/services/UsuarioServiceImpl.java
@@ -0,0 +1,113 @@
+package com.ntendencia.services;
+
+import com.ntendencia.DTO.UsuarioDTO;
+import com.ntendencia.DTO.UsuarioNewDTO;
+import com.ntendencia.domain.Cidade;
+import com.ntendencia.domain.Endereco;
+import com.ntendencia.domain.Usuario;
+import com.ntendencia.domain.enums.SexoUsuario;
+import com.ntendencia.repositories.EnderecoRepository;
+import com.ntendencia.repositories.UsuarioRepository;
+import com.ntendencia.services.exceptions.IntegridadeDeDadosException;
+import com.ntendencia.services.exceptions.ObjetoNaoEncontradoException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    public Usuario buscarUsuarioPorId(Integer id) {
+        Optional<Usuario> usuario;
+        usuario = usuarioRepository.findById(id);
+        if (usuario == null) {
+            throw new ObjetoNaoEncontradoException(
+               Utils.getMensagemValidacao("usuario.nao.encontrado", id, Usuario.class.getName())
+            );
+        }
+        return usuario.orElse(null);
+    }
+
+    @Transactional
+    public Usuario inserirNovoUsuario(Usuario obj) {
+        obj.setId(null);
+        obj = usuarioRepository.save(obj);
+        enderecoRepository.saveAll(obj.getEnderecos());
+        return obj;
+    }
+
+    public Usuario atualizarUsuario(Usuario obj) {
+        Usuario newObj = buscarUsuarioPorId(obj.getId());
+        atualizaObjeto(newObj, obj);
+        return usuarioRepository.save(newObj);
+    }
+
+    public void excluirUsuario(Integer id) {
+        buscarUsuarioPorId(id);
+        try {
+            usuarioRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new IntegridadeDeDadosException(Utils.getMensagemValidacao("excluir.usuario.com.endereco"));
+        }
+    }
+
+    public List<Usuario> buscarTodosOsUsuarios() {
+
+        return usuarioRepository.findAll();
+    }
+
+
+    public Page<Usuario> buscaPaginadaDeUsuario(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        return usuarioRepository.findAll(pageRequest);
+    }
+
+    public Usuario inserirObjetoPeloDTO(UsuarioDTO objDto) {
+
+        return modelMapper.map(objDto, Usuario.class);
+    }
+
+    public Usuario inserirObjetoPeloDTO(UsuarioNewDTO objDto) {
+        Usuario usuario = modelMapper.map(objDto, Usuario.class);
+
+        Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+
+        Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+                objDto.getBairro(), objDto.getCep(), usuario, cid);
+
+        usuario.getEnderecos().add(end);
+
+        return usuario;
+    }
+
+    private void atualizaObjeto(Usuario newObj, Usuario obj) {
+        newObj.setNome(obj.getNome());
+    }
+
+    public List<Usuario> buscarUsuariosFiltrados(String nome, String cpf, String dataNascimento, SexoUsuario sexo) {
+        List<Usuario> usuario = usuarioRepository.findBy(nome, cpf, dataNascimento, sexo);
+        if (usuario == null) {
+            throw new ObjetoNaoEncontradoException(
+                    Utils.getMensagemValidacao("usuario.nao.encontrado", nome, Usuario.class.getName()));
+        }
+        return usuario;
+    }
+
+
+}
