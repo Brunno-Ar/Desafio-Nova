6b5c8639ede2df3f5e32bdc9ce5ca8687e7a7daf
Bruno Araujo <brunnoaraujoc@gmail.com>
2023-05-04 14:15:01 -0300

    adicionando webconfig

---
+++ src/main/java/com/ntendencia/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.ntendencia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+}
